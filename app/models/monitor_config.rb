a257855e80d6ded39a7d4e147b0bfa14d201fda8
pguo <pguo@yottaa.com>
2011-09-21 13:55:47 +0800

    reorganize mongodb collections

--- app/models/monitor_config.rb
+++ app/models/monitor_config.rb
@@ -1,9 +1,10 @@
 class MonitorConfig
   include MongoMapper::Document
-  
-  require_dependency 'monitor/scheduling.rb'
+
+  after_create :initialize_tasks
 
   belongs_to :group
+
   key :name, String, :required => true
   key :url, String, :required => true
   key :frequency, Integer, :default => 10.minutes
@@ -11,4 +12,26 @@ class MonitorConfig
   key :locations, Array
   key :bandwidth
 
+  many :tasks, :class_name => "MonitorConfigTask"
+
+  def location_frequency
+    (frequency * locations.count).seconds
+  end
+
+  private
+  def initialize_tasks
+    unless locations.empty?
+      current_time = Time.now.utc
+      locations.each_with_index do |location, index|
+        last_scheduled_at = current_time + (index * frequency).seconds
+        task = MonitorConfigTask.new(
+          :next_scheduled_at => last_scheduled_at,
+          :location => location,
+          :browser => browser,
+          :realtime => false
+        )
+        self.tasks << task
+      end
+    end
+  end
 end
--- app/models/monitor_config_task.rb
+++ app/models/monitor_config_task.rb
@@ -1,10 +1,59 @@
 class MonitorConfigTask
-  include MongoMapper::EmbeddedDocument
-  
-  key :last_scheduled_at
+  include MongoMapper::Document
+
+  belongs_to :monitor_config
+
+  key :location
   key :next_scheduled_at
-  
-  key :last_scheduled_location
-  key :next_scheduled_location
-  
+  key :realtime
+  key :browser
+  key :scheduling, Boolean, :default => false
+
+  def as_json
+    {
+      :url => monitor_config.url,
+      :task => 'webpage',
+      :id => id,
+      :network_connection => monitor_config.bandwidth,
+      :browser => browser
+    }
+  end
+
+  class << self
+    def next!(params)
+      # add lock to the task
+      begin
+        task = self.collection.find_and_modify(
+          :query  => {
+            "location" => params[:location],
+            "browser" => {'$in' => params[:browsers]},
+            "scheduling" => false,
+            "next_scheduled_at" => {"$lt" => Time.now.utc}
+          },
+          :sort   => [ ["next_scheduled_at", 1] ], # ascending
+          :update => { '$set' => { "scheduling" => true } }
+        )
+      rescue Mongo::OperationFailure => e
+        return nil
+      end
+
+      # calculate after scheduled data, update and unlock the task
+      begin
+        task = self.find(task['_id'])
+        update_params = {"scheduling" => false, "next_scheduled_at" => task.next_scheduled_at + task.monitor_config.location_frequency}
+        self.collection.find_and_modify(
+          :query => {"_id" => task.id},
+          :sort   => [ ["next_scheduled_at", 1] ], # ascending
+          :update => {"$set" => update_params }
+        )
+        task.as_json
+      rescue Exception => e
+        self.collection.find_and_modify(
+          :query => {"_id" => task.id},
+          :update => {"$set" => {"scheduling" => false}}
+        )
+        nil
+      end
+    end
+  end
 end
